8e6fb8de51c80a6d1151180822c2159299e4b9d5
lug-robin <luger.robin@outlook.de>
2024-02-12 13:34:11 +0100

    try

--- shellscripts/script.sh
+++ shellscripts/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# hacky way to make shure the script 
+# hacky way to make sure the script 
 # gets allways run from parent-dir 
 # so relative paths get resolved the righ way
 script_location_dir="${BASH_SOURCE[0]}"
 cd $script_location_dir && cd ..
 # run scripts
-./shellscripts/dl_imprint.sh
-./shellscripts/dl_saxon.sh
-./shellscripts/dl_staticsearch.sh
+sudo ./shellscripts/dl_imprint.sh
+sudo ./shellscripts/dl_saxon.sh
+sudo ./shellscripts/dl_staticsearch.sh
